12e985dd9d05e6cd2f0758e5ea124918607a0831
takachan0012 <jj4068851@gmail.com>
2024-06-13 23:11:36 +0700

    add: migration profile_desa

---
+++ database/migrations/2024_06_13_160551_profile_desa.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profile_desa', function (Blueprint $table) {
+            $table->id();
+            $table->text('content_misi')->nullable();
+            $table->text('content_visi')->nullable();
+            $table->text('about_us')->nullable();
+            $table->string('yt_link')->nullable();
+            $table->string('fb_link')->nullable();
+            $table->string('wa_link')->nullable();
+            $table->string('ig_link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
