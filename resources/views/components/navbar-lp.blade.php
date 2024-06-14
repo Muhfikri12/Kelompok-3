5e0d00b22588c689ff8779db160d39a9ff2c3d40
takachan0012 <jj4068851@gmail.com>
2024-06-14 22:33:28 +0700

    feat: added navbar-lp component

---
+++ resources/views/components/navbar-lp.blade.php
@@ -0,0 +1,21 @@
+<header id="header" class="fixed-top  header-transparent ">
+    <div class="container d-flex align-items-center justify-content-between">
+
+        <div class="logo">
+            <h1><a href="index.html">[Nama Desa]</a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+        </div>
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto active" href="#hero">Tentang Kami</a></li>
+                <li><a class="nav-link scrollto" href="#features">Visi & Misi</a></li>
+                <li><a class="nav-link scrollto" href="#gallery">Sejarah</a></li>
+                <li><a class="nav-link scrollto" href="#pricing">Demografi</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+    </div>
+</header><!-- End Header -->
