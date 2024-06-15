37102fac4cb048b27962cedc0e84dea3cd442932
takachan0012 <jj4068851@gmail.com>
2024-06-16 01:43:38 +0700

    feat: added footer-lp component

--- resources/views/landing_page/sejarah.blade.php
+++ resources/views/landing_page/sejarah.blade.php
@@ -20,23 +20,6 @@
                 </div>
             </div>
         </div>
-        <div class="footer-top">
-            <div class="container">
-                <div class="row">
-                    <h3>Jejaring Sosial Kami</h3>
-                    <p>Kami mengundang Anda untuk terhubung dengan Desa [Nama Desa] melalui jejaring sosial.
-                        Dapatkan informasi terbaru, berita terkini, dan acara menarik langsung dari desa kami. Ikuti
-                        dan bergabunglah dengan komunitas online kami di platform berikut:
-                    </p>
-                    <div class="social-links mt-3">
-                        <a href="#" class="youtube"><i class="bx bxl-youtube rounded"></i></a>
-                        <a href="#" class="instagram"><i class="bx bxl-instagram rounded"></i></a>
-                        <a href="#" class="facebook"><i class="bx bxl-facebook rounded"></i></a>
-                        <a href="#" class="whatsaap"><i class="bx bxl-whatsapp rounded"></i></a>
-                    </div>
-
-                </div>
-            </div>
-        </div>
+        <x-footer-lp />
     </section><!-- End Details Section -->
 </x-root-lp>
