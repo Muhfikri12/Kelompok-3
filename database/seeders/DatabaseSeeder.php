ebf386edc25e0fc479c24fb13d65e68565b37294
takachan0012 <jj4068851@gmail.com>
2024-06-19 13:41:03 +0700

    feat: update seeder profile_desa

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\ProfileDesa;
+use App\ProfileDesas;
 use App\User;
 use Illuminate\Database\Seeder;
 
@@ -23,16 +23,21 @@ class DatabaseSeeder extends Seeder
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
 
-        ProfileDesa::create([
+        ProfileDesas::create([
             'content_misi' => 'Lorem ipsum',
             'content_visi' => 'Lorem ipsum',
-            'alamat' => 'Lorem ipsum',
-            'about_us' => 'Lorem ipsum',
-            'sejarah' => 'Lorem ipsum',
-            'yt_link' => 'Lorem ipsum',
-            'fb_link' => 'Lorem ipsum',
-            'wa_link' => 'Lorem ipsum',
-            'ig_link' => 'Lorem ipsum'
+            'alamat' => 'Desa [Nama Desa] terletak di wilayah [Nama Kecamatan, Kabupaten/Kota], provinsi [Nama Provinsi].
+                        Secara geografis, desa kami berada di koordinat [sebutkan koordinat atau letak geografis desa],
+                        yang memberikan kami pemandangan alam yang luar biasa dan iklim yang nyaman.',
+            'about_us' => 'Desa [Nama Desa] adalah sebuah desa yang terletak di [Lokasi atau Kabupaten/Kota], yang terkenal
+                        dengan keindahan alamnya, budaya yang kaya, dan masyarakat yang ramah. Desa kami menawarkan
+                        pemandangan yang memukau dan kehidupan yang harmonis, menjadikannya tempat yang ideal untuk
+                        tinggal dan berkunjung',
+            'sejarah' => 'Desa [Nama Desa] didirikan pada tahun [tahun pendirian] oleh sekelompok keluarga yang berasal dari [asal-usul pendiri]. Mereka memilih lokasi ini karena kesuburan tanahnya dan akses air yang melimpah dari sungai [nama sungai]. Seiring berjalannya waktu, desa ini berkembang menjadi pusat pertanian dan perdagangan lokal, dengan penduduk yang bekerja sama membangun infrastruktur dasar seperti jalan dan irigasi. Tradisi dan adat istiadat yang dibawa oleh para pendiri tetap dilestarikan hingga kini, menjadikan Desa [Nama Desa] sebagai komunitas yang kaya akan budaya dan sejarah yang kuat.',
+            'yt_link' => 'https://www.youtube.com/',
+            'fb_link' => 'https://web.facebook.com/',
+            'wa_link' => 'wa.me/0831918',
+            'ig_link' => 'https://www.instagram.com/'
         ]);
     }
 }
