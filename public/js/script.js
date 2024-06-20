2055913ede2393bd9b7021f865104f6c8915b5a8
takachan0012 <jj4068851@gmail.com>
2024-06-20 14:49:30 +0700

    feat: Added search placeholder

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,16 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('.datatable').DataTable({
         responsive: true,
-        ordering : false,
+        ordering: false,
         columnDefs: [
             { targets: '_all', className: 'text-center' }
         ],
         language: {
             search: "Cari : ",
+            searchPlaceholder: "Tahun",
             lengthMenu: " _MENU_ Data",
-            ordering : false,
+            ordering: false,
             info: "Data _START_ - _END_ dari _TOTAL_ data",
         },
     });
