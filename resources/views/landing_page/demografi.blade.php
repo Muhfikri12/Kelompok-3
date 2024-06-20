08c0bd5e80727d0f6f7339595ff86f120d591b70
takachan0012 <jj4068851@gmail.com>
2024-06-20 14:11:59 +0700

    feat: Added view demografi

--- resources/views/landing_page/demografi.blade.php
+++ resources/views/landing_page/demografi.blade.php
@@ -1,286 +1,80 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-    <title>Desa Hegarmanah</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
-
-    <!-- Favicons -->
-    <link href="assets/img/favicon.png" rel="icon">
-    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-    <!-- Google Fonts -->
-    <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-        rel="stylesheet">
-
-    <!-- Vendor CSS Files -->
-
-    <link href="{{ asset('assets/vendor/aos/aos.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/aos/aos.css') }}" rel="stylesheet">
-    <link href= "{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
-
-    <!-- Template Main CSS File -->
-    <link href={{ asset('assets/css/style.css') }} rel="stylesheet">
-
-    {{-- Asset Datatable --}}
-
-    <link rel="stylesheet" href="{{ asset('css/dataTables.bootstrap4.css') }}">
-    <script defer src="{{ asset('js/dataTables.js') }}"></script>
-    <script defer src="{{ asset('js/dataTables.bootstrap4.js') }}"></script>
-    <script defer src="{{ asset('js/script.js') }}"></script>
-
-    <!-- Fonts -->
-    {{-- <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
-     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-     <!-- Styles -->
-     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet"> --}}
-
-    <!-- Favicon -->
-    <link href="{{ asset('img/favicon.png') }}" rel="icon" type="image/png">
-
-    @stack('css')
-</head>
-
-<body>
-
-    <!-- ======= Header ======= -->
-    <header id="header" class="fixed-top header-transparent">
-        <div class="d-flex align-items-center justify-content-between container">
-
-            <div class="logo">
-                <h1><a href="index.html">Appland</a></h1>
-                <!-- Uncomment below if you prefer to use an image logo -->
-                <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-            </div>
-
-            <nav id="navbar" class="navbar">
-                <ul>
-                    <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-                    <li><a class="nav-link scrollto" href="#features">App Features</a></li>
-                    <li><a class="nav-link scrollto" href="#gallery">Gallery</a></li>
-                    <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
-                    <li><a class="nav-link scrollto" href="{{ route('data.demografi') }}">Demografi</a></li>
-                    <li><a class="nav-link scrollto" href="#faq">F.A.Q</a></li>
-                    <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-                        <ul>
-                            <li><a href="#">Drop Down 1</a></li>
-                            <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i
-                                        class="bi bi-chevron-right"></i></a>
-                                <ul>
-                                    <li><a href="#">Deep Drop Down 1</a></li>
-                                    <li><a href="#">Deep Drop Down 2</a></li>
-                                    <li><a href="#">Deep Drop Down 3</a></li>
-                                    <li><a href="#">Deep Drop Down 4</a></li>
-                                    <li><a href="#">Deep Drop Down 5</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Drop Down 2</a></li>
-                            <li><a href="#">Drop Down 3</a></li>
-                            <li><a href="#">Drop Down 4</a></li>
-                        </ul>
-                    </li>
-                    <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-                    <li><a class="getstarted scrollto" href="#features">Login</a></li>
-                </ul>
-                <i class="bi bi-list mobile-nav-toggle"></i>
-            </nav><!-- .navbar -->
-
-        </div>
-    </header><!-- End Header -->
-
-    <!-- ======= Hero Section ======= -->
-    <section id="hero" class="d-flex align-items-center">
-
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 d-lg-flex flex-lg-column justify-content-center align-items-stretch pt-lg-0 order-lg-1 order-2 pt-5"
-                    data-aos="fade-up">
-                    <div>
-                        <h1>App landing page template</h1>
-                        <h2>Lorem ipsum dolor sit amet, tota senserit percipitur ius ut, usu et fastidii forensibus
-                            voluptatibus. His ei nihil feugait</h2>
-                        <a href="#" class="download-btn"><i class="bx bxl-play-store"></i> Google Play</a>
-                        <a href="#" class="download-btn"><i class="bx bxl-apple"></i> App Store</a>
-                    </div>
-                </div>
-                <div class="col-lg-6 d-lg-flex flex-lg-column align-items-stretch order-lg-2 hero-img order-1"
-                    data-aos="fade-up">
-                    <img src="{{ asset('assets/img/hero-img.png') }}" class="img-fluid" alt="">
-                </div>
-            </div>
-        </div>
-
-    </section><!-- End Hero -->
-
+<x-root-lp>
+    <x-navbar-lp />
     {{-- Section Deskripsi Demografi --}}
- <section id="details" class="details">
-    <div class="container">
+    <section id="details" class="details">
+        <div class="container">
 
-        <div class="row content">
-            <div class="col-md-4 order-1 order-md-2" data-aos="fade-left">
-                <img src="{{asset('assets/img/details-2.png')}}" class="img-fluid" alt="">
+            <div class="row content">
+                <div class="col-md-4 order-1 order-md-2" data-aos="fade-left">
+                    <img src="{{ asset('assets/img/details-2.png') }}" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-8 pt-5 order-2 order-md-1" data-aos="fade-up">
+                    <h3>Demografi Desa Hegarmanah</h3>
+                    <p>
+                        Gambaran Demografi merupakan gambaran kependudukan dalam arti luas terhadap perkembangan dan
+                        persebarannya. Oleh karena itu jumlah penduduk yang besar merupakan modal dasar yang efektif
+                        bagi pembangunan yang berkelanjutan. Jumlah penduduk yang besar dan berkualitas baik dengan
+                        pertumbuhan yang teratur akan menjadi dasar dalam mencapai tujuan pembangunan.
+                    </p>
+                </div>
             </div>
-            <div class="col-md-8 pt-5 order-2 order-md-1" data-aos="fade-up">
-                <h3>Demografi Desa Hegarmanah</h3>
-                <p>
-                    Gambaran Demografi merupakan gambaran kependudukan dalam arti luas terhadap perkembangan dan persebarannya. Oleh karena itu jumlah penduduk yang besar merupakan modal dasar yang efektif bagi pembangunan yang berkelanjutan. Jumlah penduduk yang besar dan berkualitas baik dengan pertumbuhan yang teratur akan menjadi dasar dalam mencapai tujuan pembangunan.
-                </p>
-            </div>
+
+
         </div>
+    </section>
+    <!-- End Details Section -->
 
-
-    </div>
-</section>
-<!-- End Details Section -->
-
-    <main id="main">
-        <div id="demografi">
-            <div class="demografi-newsletter">
-                <div class="container">
-                    @foreach ($results as $kategori => $years)
-                    <div class="card mt-4">
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                    {{ strtoupper('Tabel Jumlah Penduduk berdasarkan ' . $kategori) }}
-                            </h5>
-                            <table class="table-bordered table-stripped display nowrap datatable table"
-                                style="width: 100%">
-                                <thead>
-                                    <tr class="table-secondary">
-                                        <th>{{__('Tahun')}}</th>
-                                        <th>{{ strtoupper($kategori) }}</th>
-                                        <th>{{__('Jumlah')}}</th>
-                                        <th>Prosentase</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($years as $year => $items)
-                                        @foreach ($items as $item)
-                                            <tr>
-                                                <td>{{ $item->tahun ?? '-' }}</td>
-                                                <td>{{ $item->keterangan ?? '-' }}</td>
-                                                <td>{{ $item->total . ' ' . $item->satuan ?? '-' }}</td>
-                                                <td>{{ round(($item->total / $items->sum('total')) * 100, 2) . ' %' }}</td>
+    <section class="details">
+        <main id="main">
+            <div id="demografi">
+                <div class="demografi-newsletter">
+                    <div class="container">
+                        @foreach ($results as $kategori => $years)
+                            <div class="card mt-4">
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        {{ strtoupper('Tabel Jumlah Penduduk berdasarkan ' . $kategori) }}
+                                    </h5>
+                                    <table class="table-bordered table-stripped display nowrap datatable table"
+                                        style="width: 100%">
+                                        <thead>
+                                            <tr class="table-secondary">
+                                                <th>{{ __('Tahun') }}</th>
+                                                <th>{{ strtoupper($kategori) }}</th>
+                                                <th>{{ __('Jumlah') }}</th>
+                                                <th>Prosentase</th>
                                             </tr>
-                                        @endforeach
-                                        <tr class="table-secondary">
-                                            <td>{{$year}}</td>
-                                            <td>Total</td>
-                                            <td>{{$items->sum('total') . " " . $items->first()->satuan ?? ''}}</td>
-                                            <td>100%</td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($years as $year => $items)
+                                                @foreach ($items as $item)
+                                                    <tr>
+                                                        <td>{{ $item->tahun ?? '-' }}</td>
+                                                        <td>{{ $item->keterangan ?? '-' }}</td>
+                                                        <td>{{ $item->total . ' ' . $item->satuan ?? '-' }}</td>
+                                                        <td>{{ round(($item->total / $items->sum('total')) * 100, 2) . ' %' }}
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                                <tr class="table-secondary">
+                                                    <td>{{ $year }}</td>
+                                                    <td>Total</td>
+                                                    <td>{{ $items->sum('total') . ' ' . $items->first()->satuan ?? '' }}
+                                                    </td>
+                                                    <td>100%</td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
-                    @endforeach
                 </div>
+
             </div>
-
-        </div>
-    </main>
-    <!-- End #main -->
-
-    <!-- ======= Footer ======= -->
-    <footer id="footer">
-        <div class="footer-top">
-            <div class="container">
-                <div class="row">
-
-                    <div class="col-lg-3 col-md-6 footer-contact">
-                        <h3>Appland</h3>
-                        <p>
-                            A108 Adam Street <br>
-                            New York, NY 535022<br>
-                            United States <br><br>
-                            <strong>Phone:</strong> +1 5589 55488 55<br>
-                            <strong>Email:</strong> info@example.com<br>
-                        </p>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 footer-links">
-                        <h4>Useful Links</h4>
-                        <ul>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 footer-links">
-                        <h4>Our Services</h4>
-                        <ul>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 footer-links">
-                        <h4>Our Social Networks</h4>
-                        <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
-                        <div class="social-links mt-3">
-                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-
-        <div class="container py-4">
-            <div class="copyright">
-                &copy; Copyright <strong><span>Appland</span></strong>. All Rights Reserved
-            </div>
-            <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/ -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-            </div>
-        </div>
-    </footer>
-    <!-- End Footer -->
-
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-            class="bi bi-arrow-up-short"></i></a>
-
-    <!-- Vendor JS Files -->
-    <script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
-    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
-
-    <!-- Template Main JS File -->
-    <script src="{{ asset('assets/js/main.js') }}"></script>
-
-    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-    {{-- <script src="{{ asset('js/sb-admin-2.min.js') }}"></script> --}}
-    {{-- @stack('js') --}}
-
-</body>
-
-</html>
+        </main>
+        <!-- End #main -->
+        <x-footer-lp />
+    </section>
+</x-root-lp>
