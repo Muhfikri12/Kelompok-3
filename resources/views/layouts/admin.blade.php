fe23182e0d7b82dac0699be1861416f8820818d4
irfan <irfan.pragma@gmail.com>
2024-06-26 21:00:59 +0700

    fix topbar dashboard

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -21,7 +21,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/adminStyle.css') }}">
 
     {{-- Asset CSS Datatable  --}}
     <link rel="stylesheet" href="{{ asset('css/dataTables.bootstrap4.css') }}">
@@ -249,7 +249,12 @@
             <div id="content">
 
                 <!-- Topbar -->
-               <nav class="navbar navbar-expand navbar-light topbar static-top mb-4 bg-white shadow">
+                <nav class="navbar navbar-expand navbar-light topbar static-top mb-4 bg-white shadow">
+
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
 
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
@@ -259,17 +264,18 @@
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span
-                                    class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
+                                    class="d-none d-lg-inline small mr-2 text-gray-600">{{ Auth::user()->name }}</span>
                                 <figure class="img-profile rounded-circle avatar font-weight-bold"
                                     data-initial="{{ Auth::user()->name[0] }}"></figure>
                             </a>
                             <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                            <div class="dropdown-menu dropdown-menu-right animated--grow-in shadow"
                                 aria-labelledby="userDropdown">
                                 <a class="dropdown-item" href="{{ route('profile') }}">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     {{ __('Profile') }}
                                 </a>
+
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#" data-toggle="modal"
                                     data-target="#logoutModal">
