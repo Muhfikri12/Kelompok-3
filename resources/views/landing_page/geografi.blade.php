65abbf9b22b241f69806180c06c84f59cf6a80af
takachan0012 <jj4068851@gmail.com>
2024-06-26 10:53:00 +0700

    feat: fixed responsive data-geografi

--- resources/views/landing_page/geografi.blade.php
+++ resources/views/landing_page/geografi.blade.php
@@ -4,16 +4,16 @@
     <section id="details" class="details">
         <div class="container pt-5">
             <div class="row">
-                <div class="col-sm-12 col-md-6 col-lg-6 order-md-2 order-1 mt-2 p-2" data-aos="fade-left">
+                <div class="col-md-6 order-md-2 mb-3" data-aos="fade-left">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43402.70622949173!2d106.78946238414318!3d-6.926033606711491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6830d1c4732cc5%3A0x14e9fd7726ffb468!2sHegarmanah%2C%20Kec.%20Cicantayan%2C%20Kabupaten%20Sukabumi%2C%20Jawa%20Barat!5e1!3m2!1sid!2sid!4v1719209176352!5m2!1sid!2sid"
-                        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                        width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
                         referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div class="col-sm-12 col-md-6 col-lg-6 order-md-1 order-2 mt-1" data-aos="fade-up">
                     <h3>Geografi Desa Hegarmanah</h3>
                     <p>
-                        {{$record->description ?? ''}}
+                        {{ $record->description ?? '' }}
                     </p>
                     <table>
                         <tr>
@@ -62,7 +62,7 @@
                         </tr>
                         <tr>
                             <td style="width:150px;">
-                               Batas Selatan
+                                Batas Selatan
                             </td>
                             <td style="width: 20px;">
                                 :
