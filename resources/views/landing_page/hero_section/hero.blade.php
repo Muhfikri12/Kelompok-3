c9f147fafde53fcf24f697839bedeaadd9b87272
takachan0012 <jj4068851@gmail.com>
2024-07-01 11:41:45 +0700

    feat: fix carousel button navigation

--- resources/views/landing_page/hero_section/hero.blade.php
+++ resources/views/landing_page/hero_section/hero.blade.php
@@ -1,35 +1,29 @@
-<div id="mySlide" class="carousel slide" data-ride="carousel">
-
-    <ol class="carousel-indicators">
-      @foreach($banner as $key => $item)
-      <li data-target="#mySlide" data-slide-to="{{$key == 0 ? 'active' : ''}}" class="active"></li>
-      @endforeach
-    </ol>
-    @foreach ($banner as $key => $item)
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        @foreach ($banner as $key => $item)
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="{{ $key }}"
+                aria-current="@if ($loop->first) true @endif"
+                class="@if ($loop->first) active @endif"></button>
+        @endforeach
+    </div>
     <div class="carousel-inner">
-        <div class="carousel-item {{$key == 0 ? 'active' : ''}}"  id="slide{{$key}}">
-            <img
-            class="d-block w-100"
-            src="{{ asset('storage/banner/' . $item->photo) }}"
-            alt="{{ $item->judul ?? 'Slide ' . $key }}"
-          />
-          <div class="carousel-caption d-none d-md-block">
-            <h5>{{$item->judul ?? ''}}</h5>
-            <p>{{$item->description ?? ''}}</p>
-          </div>
-        </div>
-    @endforeach
-
-
-    <a class="carousel-control-prev" href="#mySlide" data-slide="prev">
-      <span class="carousel-control-prev-icon"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#mySlide" data-slide="next">
-      <span class="carousel-control-next-icon"></span>
-      <span class="sr-only">Next</span>
-    </a>
-
-  </div>
-
+        @foreach ($banner as $key => $item)
+            <div class="carousel-item @if ($loop->first) active @endif">
+                <img class="d-block w-100" src="{{ asset('storage/banner/' . $item->photo) }}"
+                    alt="{{ $item->judul ?? 'Slide ' . $key }}" />
+                <div class="carousel-caption d-md-block">
+                    <h5>{{ $item->judul ?? '' }}</h5>
+                    <p class="line-clamp">{{ $item->description ?? '' }}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
 </div>
