f7b27167d51b6d73d79db54bae3fac3f88f766bd
takachan0012 <jj4068851@gmail.com>
2024-07-01 14:40:59 +0700

    feat: Added modal scrollable

--- resources/views/landing_page/hero_section/hero.blade.php
+++ resources/views/landing_page/hero_section/hero.blade.php
@@ -11,9 +11,28 @@
             <div class="carousel-item @if ($loop->first) active @endif">
                 <img class="d-block w-100" src="{{ asset('storage/banner/' . $item->photo) }}"
                     alt="{{ $item->judul ?? 'Slide ' . $key }}" />
-                <div class="carousel-caption d-md-block">
+                <button class="carousel-caption d-md-block" data-bs-toggle="modal"
+                    data-bs-target="#{{ $key }}">
                     <h5>{{ $item->judul ?? '' }}</h5>
                     <p class="line-clamp">{{ $item->description ?? '' }}</p>
+                </button>
+                <div class="modal fade" id="{{ $key }}" tabindex="-1" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-scrollable">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5>{{ $item->judul ?? '' }}</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <p>{{ $item->description ?? '' }}</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         @endforeach
