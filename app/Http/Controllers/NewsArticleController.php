561df44802787eb499ba4635117d9ba8b825e69f
User <fikrisacila0@gmail.com>
2024-07-02 10:39:31 +0700

    make validation for berita

--- app/Http/Controllers/NewsArticleController.php
+++ app/Http/Controllers/NewsArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\NewsRequest;
 use Carbon\Carbon;
 use App\Models\News;
 // use App\Models\article;
@@ -126,7 +127,7 @@ class NewsArticleController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(NewsRequest $request)
     {
         if ($request->hasFile('image_content')) {
             $image = $request->file('image_content');
--- resources/views/dashboard-admin-article/news/create_news.blade.php
+++ resources/views/dashboard-admin-article/news/create_news.blade.php
@@ -1,5 +1,4 @@
 <div class="content-dashboard-article">
-
     <div class="form-content-dasboard-article">
         <h3 class="font-weight-bold text-primary m-0">{{ $title ?? __('Membuat Acara') }}</h3>
         <hr>
@@ -8,30 +7,63 @@
             <table>
                 <tr>
                     <td><label for="event_news">Judul Berita</label></td>
-                    <td><input type="text" id="event_news" name="event_news"></td>
+                    <td><input type="text" id="event_news" name="event_news"
+                            @error('event_news')
+                        is-invalid
+                    @enderror>
+                        @error('event_news')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </td>
                 </tr>
                 <tr>
                     <td><label for="event_cat">Kategori Berita</label></td>
                     <td><select style="padding: 2px 10px; border-radius: 10px; height: 2.1rem; width: 100%;"
                             id="event_cat" name="event_cat">
+                            <option value="" selected>-- Pilih --</option>
                             @foreach ($category as $item)
-                                <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                <option value="{{ $item->id }}" {{ old('event_cat') ? 'selected' : '' }}>
+                                    {{ $item->name }}</option>
                             @endforeach
-                        </select></td>
+                        </select>
+                        @error('event_cat')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </td>
 
                 </tr>
                 <tr>
                     <td><label for="headline_news">Headline Berita</label></td>
-                    <td><input type="text" id="headline_news" name="headline_news" required> <br></td>
+                    <td><input type="text" id="headline_news" name="headline_news"
+                            @error('headline_news')
+                        is-invalid
+                    @enderror>
+                        @error('headline_news')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </td>
                 </tr>
                 <tr>
                     <td><label for="image_content">Masukan Gambar</label></td>
-                    <td><input type="file" id="image_content" name="image_content"></td>
+                    <td><input type="file" id="image_content" name="image_content"
+                            @error('image_content')
+                        is-invalid
+                    @enderror>
+                        @error('image_content')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </td>
                 </tr>
                 <tr>
-                    <td><label for="detail_news">Detail Acara</label></td>
+                    <td><label for="detail_news">Detail Berita</label></td>
                     <td>
-                        <textarea id="detail_content" name="detail_news"></textarea>
+                        <textarea id="detail_content" name="detail_news"
+                            @error('detail_news')
+                        is-invalid
+                    @enderror></textarea>
+                        @error('detail_news')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
                     </td>
                     <td><input type="hidden" name="type" value="berita"></td>
                 </tr>
