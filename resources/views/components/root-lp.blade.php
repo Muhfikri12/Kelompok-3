b3f6c6ef9ddd71d7a57e924d827131e4f6587035
irfan <irfan.pragma@gmail.com>
2024-07-02 09:41:00 +0700

    fix : sync logo desa

--- resources/views/components/root-lp.blade.php
+++ resources/views/components/root-lp.blade.php
@@ -10,7 +10,7 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="{{ asset('img/jabar.png') }}" rel="icon">
+    <link href="{{ asset('assets/img/logo_1.png') }}" rel="icon">
     <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -18,7 +18,7 @@
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
 
     <!-- Favicon -->
-    <link href="{{ asset('img/jabar.png') }}" rel="icon" type="image/png">
+    <link href="{{ asset('assets/img/logo_1.png') }}" rel="icon" type="image/png">
 </head>
 <body class="bg-gradient-primary min-vh-100 d-flex justify-content-center align-items-center">
 
